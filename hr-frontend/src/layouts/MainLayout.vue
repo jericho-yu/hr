a17ba082f36f0aedc2fad86cd3c9e5069adf133a
Jericho Yu <jerichoyu@JerichodeMac-mini-2.local>
2025-06-20 10:01:04 +0800

    [feature]优化菜单

--- hr-frontend/src/layouts/MainLayout.vue
+++ hr-frontend/src/layouts/MainLayout.vue
@@ -1,68 +1,69 @@
 <template>
-    <q-layout view="hHh lpR fFf">
-        <q-header bordered class="bg-primary text-white" height-hint="98">
-            <q-toolbar>
-                <q-btn dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
+  <q-layout view="hHh lpR fFf">
+    <q-header bordered class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
 
-                <q-toolbar-title>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-                    </q-avatar>
-                    人事：🤠💩2V
-                </q-toolbar-title>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          人事：🤠💩2V
+        </q-toolbar-title>
 
-                <!-- <q-btn dense flat round icon="menu" @click="rightDrawerOpen = !rightDrawerOpen" /> -->
-            </q-toolbar>
+        <!-- <q-btn dense flat round icon="menu" @click="rightDrawerOpen = !rightDrawerOpen" /> -->
+      </q-toolbar>
 
-            <q-tabs align="center"> </q-tabs>
-        </q-header>
+      <q-tabs align="center"> </q-tabs>
+    </q-header>
 
-        <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
-            <q-list style="min-width: 100px">
-                <q-item clickable>
-                    <q-item-section>
-                        <router-link to="/">考勤预处理</router-link>
-                    </q-item-section>
-                </q-item>
-                <q-item clickable>
-                    <q-item-section>
-                        <router-link to="/timeCalculator">时间计算器</router-link>
-                    </q-item-section>
-                </q-item>
-                <q-item clickable>
-                    <q-item-section>
-                        <router-link to="/rezip">重新压缩</router-link>
-                    </q-item-section>
-                </q-item>
-                <q-separator />
-            </q-list>
-            <!-- drawer content -->
-        </q-drawer>
+    <q-drawer v-model="leftDrawerOpen" side="left" overlay bordered>
+      <q-list link dense separator style="min-width: 200px">
+        <q-item dense clickable v-ripple @click="fnToHref(router, '/')">
+          <q-item-section avatar>考勤预处理</q-item-section>
+        </q-item>
 
-        <!-- <q-drawer v-model="rightDrawerOpen" side="right" overlay bordered> -->
-        <!-- drawer content -->
-        <!-- </q-drawer> -->
+        <q-item dense clickable v-ripple @click="fnToHref(router, '/timeCalculator')">
+          <q-item-section avatar>时间计算器</q-item-section>
+        </q-item>
 
-        <q-page-container>
-            <router-view />
-        </q-page-container>
+        <q-item dense clickable v-ripple @click="fnToHref(router, '/rezip')">
+          <q-item-section avatar>重新压缩</q-item-section>
+        </q-item>
+      </q-list>
+      <!-- drawer content -->
+    </q-drawer>
 
-        <q-footer bordered class="bg-grey-8 text-white">
-            <q-toolbar>
-                <q-toolbar-title>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
-                    </q-avatar>
-                    <div></div>
-                </q-toolbar-title>
-            </q-toolbar>
-        </q-footer>
-    </q-layout>
+    <!-- <q-drawer v-model="rightDrawerOpen" side="right" overlay bordered> -->
+    <!-- drawer content -->
+    <!-- </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+    <q-footer bordered class="bg-grey-8 text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
+          </q-avatar>
+          <div></div>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
+  </q-layout>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import { useRouter } from "vue-router"
+
+const router = useRouter()
 
 const leftDrawerOpen = ref(false)
+
+const fnToHref = (router, url) => router.push(url)
+
 // const rightDrawerOpen = ref(false)
 </script>
